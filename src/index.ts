56e9d4d7a9cd0d0d0895555493216b490591713f
PixelPaula <77841946+PixelPaula@users.noreply.github.com>
2021-07-05 11:36:57 +0200

    added signs

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.openCoWebSite('https://app.mural.co/t/diedigitalwerkstatt7758/m/diedigitalwerkstatt7758/1604866566045/3fefe79de5ae50fc636787405c52ea5261b72b0f');
+WA.openCoWebSite('https://app.mural.co/t/diedigitalwerkstatt7758/m/diedigitalwerkstatt7758/1604866566045/3fefe79de5ae50fc636787405c52ea5261b72b0f?sender=db9979 ');
