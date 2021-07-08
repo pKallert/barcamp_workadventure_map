de5e64bfcba75cb83050bebb508ee308e8fdc4c6
PixelPaula <77841946+PixelPaula@users.noreply.github.com>
2021-07-08 14:44:41 +0200

    video link changes

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.openCoWebSite('https://app.mural.co/embed/7c6eac1b-d41d-4fa1-aa34-01028f25a4ed');
+WA.openCoWebSite('https://kallertpa.github.io/barcamp_workadventure_map/map_7.html');
